db7778f88005815b60bcf223f4ca20ec7a8aa6ac
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-12-04 20:53:54 +0100

    déplacement de tout les pages php vers la racine, dossier pages supprimer, lien sur vers style.css corriger dans toute les pages

--- admin.php
+++ admin.php
@@ -20,7 +20,7 @@ $data = $request->fetch_All();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>administrateur</title>
 </head>
 <body>
